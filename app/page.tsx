140da5183a21f53d24103138d237059efab5b938
Codingwithriha <rihashehzadi2003@gmail.com>
2025-07-14 13:03:45 +0500

    inprgs: home-section

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,47 @@
-import { Button } from '@/components/ui/button'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to my Saas app</h1>
-      <Button className="mt-4">
-        Click Me
-      </Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+      <section className='home-section'>
+        <CompanionCard
+        id="123"
+        name="Neura the Brainy Explorer"
+        topic="Neural Network of the Brain"
+        subject="Science"
+        duration={45}
+        color="#ffda6e"
+        />
+        <CompanionCard
+        id="456"
+        name="Countsy the Number Wizard"
+        topic="Derivatives & Integrals"
+        subject="Science"
+        duration={30}
+        color="#e5d0ff"
+        />
+        <CompanionCard
+        id="789"
+        name="Verba the Vocabulary Builder"
+        topic="English Literature"
+        subject="Science"
+        duration={30}
+        color="#BDE7FF"
+        />
+        
+        
+      </section>
+
+      <section className='home-section'>
+        <CompanionsList />
+        <CTA />
+      </section>
+     
+    </main>
   )
 }
 
---
+++ components/CompanionsList.tsx
@@ -0,0 +1,11 @@
+
+
+type Props = {}
+
+const CompanionsList = (props: Props) => {
+  return (
+    <div>CompanionsList</div>
+  )
+}
+
+export default CompanionsList
